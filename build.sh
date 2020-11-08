ef2f787ffb67ee017c86a2c21451920a7115f2bd
vms <michail.vms@gmail.com>
2020-11-08 21:12:14 +0300

    initial

--- build.sh
+++ build.sh
@@ -14,6 +14,7 @@ cp user-list/target/wasm32-wasi/release/user-list.wasm artifacts/
 cp history/target/wasm32-wasi/release/history.wasm artifacts/
 
  cd artifacts
+ wget https://github.com/fluencelabs/sqlite/releases/download/v0.8.0_w/sqlite3.wasm
  base64 -w 0 user-list.wasm > userListBs64.ts && sed -i '1s/^/export const USER_LIST_BS64=\"/' userListBs64.ts && echo '"' >> userListBs64.ts
  base64 -w 0 history.wasm > historyBs64.ts && sed -i '1s/^/export const HISTORY_BS64=\"/' historyBs64.ts && echo '"' >> historyBs64.ts
  base64 -w 0 sqlite.wasm > sqliteBs64.ts && sed -i '1s/^/export const SQLITE_BS64=\"/' sqliteBs64.ts && echo '"' >> sqliteBs64.ts
